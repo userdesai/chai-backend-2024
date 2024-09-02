24ee2c61f1b53811bd28e99f309e61c81929c7c8
Aniket <120aniket4033@sjcem.edu.in>
2024-09-02 12:00:56 +0530

    add user model and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+
+const userSchema=new mongoose.Schema({
+  username:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+    index:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+  },
+  fullname:{
+    type:String,
+    required:true,
+    lowercase:true,
+    index:true,
+  },
+  avatar:{
+    type:String,
+    required:true
+  },
+  coverImage:{
+    type:String
+  },
+  password:{
+    type:String,
+    required:[true,"minimum 6 characters"]
+  },
+  refreshToken:{
+    type:String
+  },
+  watchHistory:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    }
+  ]
+},
+{
+    timestamps:true
+}
+)
+
+userSchema.pre('save',async function(next){
+if(!this.isModified("password")) return next();
+this.password=bcrypt.hash(this.password,10)
+next()
+})
+
+
+userSchema.methods.isPasswordCorrect=async function(password){
+  return  await  bcrypt.compare(password,this.password)
+}
+
+
+userSchema.methods.generateAccessToken=function(){
+   return jwt.sign(
+        //payload
+        {
+             _id:this._id,
+             usename:this.usename,
+             email:this.email,
+             fullName:this.fullName
+        },
+        //secret key 
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+
+        //expiry
+        {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken=function(){
+    jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET_KEY,
+        {
+            expiresIn:REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User=mongoose.model('User',userSchema)
---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+import mongooseAggrigatePaginate from 'mongoose-aggregate-paginate-v2'
+
+const videoSchema=new mongoose.Schema({
+    videoFile:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        requird:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0,
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    }
+
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggrigatePaginate)
+
+export const Video=mongoose.model("Video",videoSchema)
